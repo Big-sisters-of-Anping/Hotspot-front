0c61d64dedf8d510fbd1cbf34ab55fb0a0b390a9
kxliu <kxliu@smail.nju.edu.cn>
2020-06-23 00:03:48 +0800

    discover

--- pages/index/discover.js
+++ pages/index/discover.js
@@ -1,18 +1,34 @@
 // pages/discover.js
+
+var util = require('../../utils/util.js');
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    imgUrls: [
+      '../../images/a.jpg',
+      '../../images/b.jpg',
+      '../../images/c.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    feed: [],
+    feed_length: 0,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    this.refresh();
   },
 
   /**
@@ -66,11 +82,62 @@ Page({
 
   },
 
+  //tabbar
   switchTab: function(e){
     this.setData({
       url: "/pages/index/discover",
     });
   },
+
+  //when scroll to the top
+  upper: function () {
+    wx.showNavigationBarLoading()
+    this.refresh();
+    console.log("upper");
+    setTimeout(function(){wx.hideNavigationBarLoading();wx.stopPullDownRefresh();}, 2000);
+  },
+
+  //when scroll to the button
+  lower: function (e) {
+    wx.showNavigationBarLoading();
+    var that = this;
+    setTimeout(function(){wx.hideNavigationBarLoading();that.nextLoad();}, 1000);
+    console.log("lower")
+  },
+
+    //使用本地 fake 数据实现刷新效果
+    refresh: function(){
+      var feed = util.getDiscovery();
+      console.log("loaddata");
+      var feed_data = feed.data;
+      this.setData({
+        feed:feed_data,
+        feed_length: feed_data.length
+      });
+    },
+
+    //网络请求数据, 实现刷新
+    refresh0: function(){
+    var index_api = '';
+    util.getData(index_api)
+        .then(function(data){
+          //this.setData({
+          //
+          //});
+          console.log(data);
+        });
+  },
+
+    //使用本地 fake 数据实现继续加载效果
+    nextLoad: function(){
+      var next = util.discoveryNext();
+      console.log("continueload");
+      var next_data = next.data;
+      this.setData({
+        feed: this.data.feed.concat(next_data),
+        feed_length: this.data.feed_length + next_data.length
+      });
+  }
 })
 
 // Component({
--- utils/util.js
+++ utils/util.js
@@ -17,3 +17,38 @@ const formatNumber = n => {
 module.exports = {
   formatTime: formatTime
 }
+
+var discovery = require('../data/data_discovery.js');
+var discovery_next = require('../data/data_discovery_next.js');
+
+//used for real pull for data
+function getData(url){
+  return new Promise(function(resolve, reject){
+    wx.request({
+      url: url,
+      data: {},
+      header: {
+        //'Content-Type': 'application/json'
+      },
+      success: function(res) {
+        console.log("success")
+        resolve(res)
+      },
+      fail: function (res) {
+        reject(res)
+        console.log("failed")
+      }
+    })
+  })
+}
+
+function getDiscovery(){
+  return discovery.discovery;
+}
+
+function discoveryNext(){
+  return discovery_next.next;
+}
+
+module.exports.getDiscovery = getDiscovery;
+module.exports.discoveryNext = discoveryNext;
