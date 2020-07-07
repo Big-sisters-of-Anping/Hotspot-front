e77449616fb97c6bd545011071ab436c8e2bc3f2
tx <987901613@qq.com>
2020-07-08 01:36:03 +0800

    add aboutme and share

---
+++ pages/developer/developer.js
@@ -0,0 +1,66 @@
+// pages/developer/developer.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    info: "你好哇, 我们是来自南京大学和同济大学的开发团队：安平十三妹，立志成为代码界的扛把子！\n\n欢迎给我们发来意见和建议哦:)"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
